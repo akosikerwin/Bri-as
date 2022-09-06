16bfbf0708ee43c52e139a31e52e8abce3cc1a38
akosikerwin <111828131+akosikerwin@users.noreply.github.com>
2022-09-06 09:52:20 +0800

    Create app.java

---
+++ app.java
@@ -0,0 +1,22 @@
+public class App {
+ public static void main(String[] args) {
+
+   // Character Method 
+   student me = new student();
+
+   me.surname = "Briñas";
+   me.firstname = "JohnKerwin";
+   me.middleinitial = 'L';
+   me.dateOfBirth =  "march/18/2002";
+   me.studentNumber = 2022104771;
+   me.EmailAddress = "brinasjl@students.national-u.edu.ph";
+   me.iAmAwesome = true;
+
+    me.sayMyStudentNumber();
+    me.saMyEmailAddress();
+    me.amIAwesome();
+
+
+ }
+    }
+
